5ed4922b08e15eb06b44d79181cc98341db667ff
JoseLazaro <contato.jlazaro@gmail.com>
2021-04-16 20:06:59 -0300

    Fixing sort

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function sort() {
   var randomNumber1 = Math.ceil(Math.random() * 6);
-  var imagename = "dice" + randomNumber1 + ".png";
+  var imagename = "images/dice" + randomNumber1 + ".png";
 
   var image = document.getElementById("img1");
   image.src = imagename;
