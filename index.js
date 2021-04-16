20f753e284cff3f04033c3be427b4dd405c10901
JoseLazaro <contato.jlazaro@gmail.com>
2021-04-16 20:01:45 -0300

    Adding sort

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-var randomNumber1 = Math.ceil(Math.random() * 6)
+function sort() {
+  var randomNumber1 = Math.ceil(Math.random() * 6);
+  var imagename = "dice" + randomNumber1 + ".png";
+
+  var image = document.getElementById("img1");
+  image.src = imagename;
+}
