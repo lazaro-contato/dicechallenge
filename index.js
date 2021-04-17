14a7366948697e98a9446ca030c0bfeb6e7bbca1
JoseLazaro <contato.jlazaro@gmail.com>
2021-04-16 21:09:20 -0300

    Added all changes

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-function sort() {
+function sort1() {
   var randomNumber1 = Math.ceil(Math.random() * 6);
-  var imagename = "images/dice" + randomNumber1 + ".png";
+  var imagename1 = "images/dice" + randomNumber1 + ".png";
 
-  var image = document.getElementById("img1");
-  image.src = imagename;
+  var randomNumber2 = Math.ceil(Math.random() * 6);
+  var imagename2 = "images/dice" + randomNumber2 + ".png";
+
+  var image1 = document.getElementById("img1");
+  image1.src = imagename1;
+
+  var image2 = document.getElementById("img2");
+  image2.src = imagename2;
+
+  if (randomNumber1 > randomNumber2) {
+    var text = document.querySelector(".refresh");
+    text.innerHTML = "Number one Win";
+  } else if (randomNumber1 < randomNumber2) {
+    var text = document.querySelector(".refresh");
+    text.innerHTML = "Number two Win";
+  } else if (randomNumber1 == randomNumber2) {
+    var text = document.querySelector(".refresh");
+    text.innerHTML = "Draw!";
+  }
 }
